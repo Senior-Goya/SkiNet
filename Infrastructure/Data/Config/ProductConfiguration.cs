118ac2b5892624484ef3aa2f15eaa64d3fc89fc3
Jorge Rivas <59454582+Senior-Goya@users.noreply.github.com>
2021-10-23 19:04:44 -0400

    Added Repository pattern changes

---
+++ Infrastructure/Data/Config/ProductConfiguration.cs
@@ -0,0 +1,21 @@
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Config
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(p => p.Id).IsRequired();
+            builder.Property(p => p.Name).IsRequired().HasMaxLength(100);
+            builder.Property(p => p.Description).IsRequired();
+            builder.Property(p => p.Price).HasColumnType("decimal(18,2)");
+            builder.Property(p => p.PictureUrl).IsRequired();
+            builder.HasOne(x => x.ProductBrand).WithMany().HasForeignKey(k => k.ProductBrandId);
+            builder.HasOne(x => x.ProductType).WithMany().HasForeignKey(k => k.ProductTypeId);
+
+        }
+    }
+}
--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,4 +1,5 @@
 
+using System.Reflection;
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,5 +12,14 @@ namespace Infrastructure.Data
         }
 
         public DbSet<Product> Products { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+
+        protected  override void OnModelCreating(ModelBuilder model)
+        {
+            base.OnModelCreating(model);
+            model.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
     }
 }
